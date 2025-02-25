f0cfc83f1b1c1a2486eb736c1c89a99f51a0d3f4
Christian Alejandro Herrejon Villa <christianahvilla@gmail.com>
2025-02-24 21:45:47 -0600

    Add Production Config

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,5 +1,8 @@
 export const EnvConfiguration = () => ({
   environment: process.env.NODE_ENV || 'dev',
-  port: process.env.PORT || 3000,
+  port:
+    process.env.NODE_ENV === 'production'
+      ? undefined
+      : process.env.PORT || 3000,
   httpClient: process.env.HTTP_CLIENT || 'axios',
 });
--- src/config/joi.validation.ts
+++ src/config/joi.validation.ts
@@ -1,6 +1,10 @@
 import * as Joi from 'joi';
 
 export const JoiValidationSchema = Joi.object({
-  HTTP_CLIENT: Joi.required().default('axios'),
-  PORT: Joi.number().default(3000),
+  HTTP_CLIENT: Joi.string().default('axios'),
+  PORT: Joi.when('NODE_ENV', {
+    is: 'production',
+    then: Joi.forbidden(), // It is not used on Prod
+    otherwise: Joi.number().default(3000),
+  }),
 });
